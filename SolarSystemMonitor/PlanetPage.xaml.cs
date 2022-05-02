13bac35c03578fdda2d9dc21c95bbee18106d9bd
Ron Studený <ronstudeny@gmail.com>
2022-05-02 16:26:55 +0200

    Functional planet list view

--- SolarSystemMonitor/PlanetPage.xaml.cs
+++ SolarSystemMonitor/PlanetPage.xaml.cs
@@ -12,9 +12,35 @@ namespace SolarSystemMonitor
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PlanetPage : ContentPage
     {
+        public static List<SpaceObject> Planets;
         public PlanetPage()
         {
             InitializeComponent();
+
+            Planets = GetPlanets();
+            PlanetListView.ItemsSource = Planets;
+        }
+
+        private List<SpaceObject> GetPlanets()
+        {
+            List<SpaceObject> res = new List<SpaceObject>();
+
+            res.Add(new SpaceObject("Mercury", 1, 10, 5, SpaceObject.ObjectTypes.Planet));
+            res.Add(new SpaceObject("Venus", 1, 10, 5, SpaceObject.ObjectTypes.Planet));
+            res.Add(new SpaceObject("Earth", 1, 10, 5, SpaceObject.ObjectTypes.Planet));
+            res.Add(new SpaceObject("Mars", 1, 10, 5, SpaceObject.ObjectTypes.Planet));
+            res.Add(new SpaceObject("Jupiter", 1, 10, 5, SpaceObject.ObjectTypes.Planet));
+            res.Add(new SpaceObject("Saturn", 1, 10, 5, SpaceObject.ObjectTypes.Planet));
+            res.Add(new SpaceObject("Uranus", 1, 10, 5, SpaceObject.ObjectTypes.Planet));
+            res.Add(new SpaceObject("Neptune", 1, 10, 5, SpaceObject.ObjectTypes.Planet));
+
+            return res;
+        }
+
+        private async void PlanetTapped(object sender, ItemTappedEventArgs e)
+        {
+            SpaceObjectPage page = new SpaceObjectPage(PlanetListView.SelectedItem as SpaceObject);
+            await Navigation.PushAsync(page);
         }
     }
 }
---
+++ SolarSystemMonitor/SpaceObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarSystemMonitor
+{
+    public class SpaceObject
+    {
+        public SpaceObject(string name, double mass, double relativeSpeed, double gravitationalFroce, ObjectTypes objectType)
+        {
+            Name = name;
+            Mass = mass;
+            RelativeSpeed = relativeSpeed;
+            GravitationalFroce = gravitationalFroce;
+            ObjectType = objectType;
+        }
+
+        public string Name { get; set; }
+        public double Mass { get; set; }
+        public double RelativeSpeed { get; set; }
+        public double GravitationalFroce { get; set; }
+        public ObjectTypes ObjectType { get; set; }
+
+        public enum ObjectTypes { Star, Planet, Moon, Asteroid, Satellite }
+    }
+}
